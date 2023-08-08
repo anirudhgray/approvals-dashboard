c164d98b86e3214a0cac05147745e2d275481793
anirudh <anirudh04mishra@gmail.com>
2023-08-08 17:21:45 +0530

    feat: auth middleware

--- backend/app/src/middlewares/Authorise.ts
+++ backend/app/src/middlewares/Authorise.ts
@@ -83,3 +83,85 @@ export const authoriseAdmin = async (req: GetUserInfoRequest, res: Response, nex
     });
   }
 };
+
+export const authoriseApprover = async (req: GetUserInfoRequest, res: Response, next: NextFunction) => {
+  const jwtsecret = process.env.JWT_SECRET;
+  let token: string = req.headers.authorization;
+  if (!token) {
+    return res.status(401).json({
+      success: false,
+      error: 'No token provided.'
+    });
+  }
+  if (token.startsWith('Bearer ')) {
+    // Remove Bearer from string
+    token = token.slice(7, token.length);
+  }
+  if (token) {
+    jwt.verify(token, jwtsecret, (err: Error, decoded: IToken) => {
+      if (err) {
+        return res.status(401).json({
+          success: false,
+          error: 'Invalid Token'
+        });
+      }
+      req.user = decoded;
+      req.userId = decoded.id;
+    });
+  } else {
+    return res.status(401).json({
+      success: false,
+      error: 'Invalid Token'
+    });
+  }
+  const user: IUserModel = await User.findOne({_id:req.userId});
+  if (user.role == 1) {
+    next();
+  } else {
+    return res.status(403).json({
+      success: false,
+      error: 'You are not an approver!'
+    });
+  }
+};
+
+export const authoriseRequester = async (req: GetUserInfoRequest, res: Response, next: NextFunction) => {
+  const jwtsecret = process.env.JWT_SECRET;
+  let token: string = req.headers.authorization;
+  if (!token) {
+    return res.status(401).json({
+      success: false,
+      error: 'No token provided.'
+    });
+  }
+  if (token.startsWith('Bearer ')) {
+    // Remove Bearer from string
+    token = token.slice(7, token.length);
+  }
+  if (token) {
+    jwt.verify(token, jwtsecret, (err: Error, decoded: IToken) => {
+      if (err) {
+        return res.status(401).json({
+          success: false,
+          error: 'Invalid Token'
+        });
+      }
+      req.user = decoded;
+      req.userId = decoded.id;
+    });
+  } else {
+    return res.status(401).json({
+      success: false,
+      error: 'Invalid Token'
+    });
+  }
+  const user: IUserModel = await User.findOne({_id:req.userId});
+  if (user.role == 2) {
+    next();
+  } else {
+    return res.status(403).json({
+      success: false,
+      error: 'You are not a requester!'
+    });
+  }
+};
