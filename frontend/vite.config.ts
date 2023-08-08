232b71e1ae93fec19ae8207017f9c44af087e23d
anirudh <anirudh04mishra@gmail.com>
2023-08-09 00:23:16 +0530

    refactor: frontend init

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,20 +1,12 @@
-import { defineConfig } from "vitest/config"
+import path from "path"
 import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    open: true,
-  },
-  build: {
-    outDir: "build",
-    sourcemap: true,
-  },
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "src/setupTests",
-    mockReset: true,
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
 })
