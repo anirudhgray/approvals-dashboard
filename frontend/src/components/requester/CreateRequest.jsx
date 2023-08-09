d8aa20afd8b19f28a98b73eb86fcd86d71862714
anirudh <anirudh04mishra@gmail.com>
2023-08-09 11:48:39 +0530

    feat: file drop frontend

--- frontend/src/components/requester/CreateRequest.jsx
+++ frontend/src/components/requester/CreateRequest.jsx
@@ -2,12 +2,14 @@ import { NativeSelect, Button, Textarea, Loader } from '@mantine/core'
 import { useEffect, useState } from 'react'
 import axios from '../../../axios'
 import { toast } from 'react-toastify'
+import {Dropzone, IMAGE_MIME_TYPE, PDF_MIME_TYPE} from '@mantine/dropzone'
 
 export default function CreateRequest({workflowsList}) {
     const [description, setDescription] = useState("")
     const [workflow, setWorkflow] = useState("")
     const [selectedDesc, setSelectedDesc] = useState("")
     const [loading, setLoading] = useState("")
+    const [files, setFiles] = useState([])
 
     useEffect(() => {
         const selected = workflowsList.filter(flow => {
@@ -41,6 +43,12 @@ export default function CreateRequest({workflowsList}) {
             <NativeSelect defaultValue={"Select"} required data={workflowsList} value={workflow} label="Select Workflow" onChange={(e) => setWorkflow(e.currentTarget.value)} />
             <p className='text-sm'>{selectedDesc && `Workflow Description: ${selectedDesc}`}</p>
             <Textarea required value={description} onChange={e => setDescription(e.currentTarget.value)} label="Description" placeholder='Why are you making this request?' />
+            <Dropzone 
+                onDrop={(newfiles) => {console.log('accepted files', newfiles);setFiles([...newfiles, ...files])}}
+                onReject={(files) => console.log('rejected files', files)}
+                maxSize={3 * 1024 ** 2} className='p-16 mt-5' accept={IMAGE_MIME_TYPE}>
+                {files.length ? <p className='text-center'>Attached files: {files.map((file, i) => <span key={`file${i}`}>{file.name}, </span>)}</p> : <p className='text-center'>Drop any supporting media here (images or PDFs only)</p>}
+            </Dropzone>
             <Button type='submit' variant='outline' fullWidth mt="xl" size="md">
                 {loading ? <Loader /> :"Create"}
             </Button>
